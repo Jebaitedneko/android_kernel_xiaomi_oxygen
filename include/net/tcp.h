23a9d81f2601c5b17115cb8ac2adac6fa841857d
Jebaitedneko <16777012+Jebaitedneko@users.noreply.github.com>
2021-01-18 10:58:46 +0530

    include/: import OEM changes

--- include/net/tcp.h
+++ include/net/tcp.h
@@ -1085,7 +1085,6 @@ static inline void tcp_prequeue_init(struct tcp_sock *tp)
 }
 
 bool tcp_prequeue(struct sock *sk, struct sk_buff *skb);
-int tcp_filter(struct sock *sk, struct sk_buff *skb);
 
 #undef STATE_TRACE
 
